decf99701170a892d82c0587264acc5d177705a7
satellitestudiodesign <contact@satellitestud.io>
2018-05-07 18:23:41 +0200

    fist version

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -4,7 +4,7 @@
  * https://HosseinKarami.github.io/fastshell
  * @author Hossein Karami
  * @version 1.0.5
- * Copyright 2016. MIT licensed.
+ * Copyright 2018. MIT licensed.
  */
 (function ($, window, document, undefined) {
 
@@ -12,6 +12,56 @@
 
   $(function () {
     // FastShell
+  });
+
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('.current').removeClass('current');
+    $(this).addClass('current');
+    var offset = ($(window).height() - $($(this).attr('href')).height()) / 2 - 50;
+    if (offset < 0) { offset = 0; }
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - offset
+    }, 500);
+  });
+
+  var sidebar = $('.sidebar');
+  var main = $('.main');
+
+  function mouseMoveHandler(e) {
+    console.log("mousemove");
+    if (e.pageX < 200) {
+      sidebar.removeClass('hidden');
+      main.removeClass('full');
+    } else {
+      sidebar.addClass('hidden');
+      main.addClass('full');
+    }
+  }
+
+  function showOrHideMenu() {
+    var w = $(window).width();
+    if (w >= 1280) {
+      $(window).off('mousemove', mouseMoveHandler);
+      sidebar.removeClass('hidden');
+      main.removeClass('full');
+    } else if (w < 1280 && w > 900) {
+      $(window).off('mousemove', mouseMoveHandler)
+        .on('mousemove', mouseMoveHandler);
+    } else {
+      $(window).off('mousemove', mouseMoveHandler);
+      sidebar.addClass('hidden');
+      main.addClass('full');
+    }
+  }
+  showOrHideMenu();
+
+  $(window).on('resize', showOrHideMenu);
+
+  $(".lazy").unveil(null, function () {
+    $(this).load(function () {
+      this.style.opacity = 1;
+    });
   });
 
 })(jQuery, window, document);
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,4 +6,54 @@
     // FastShell
   });
 
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('.current').removeClass('current');
+    $(this).addClass('current');
+    var offset = ($(window).height() - $($(this).attr('href')).height()) / 2 - 50;
+    if (offset < 0) { offset = 0; }
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - offset
+    }, 500);
+  });
+
+  var sidebar = $('.sidebar');
+  var main = $('.main');
+
+  function mouseMoveHandler(e) {
+    console.log("mousemove");
+    if (e.pageX < 200) {
+      sidebar.removeClass('hidden');
+      main.removeClass('full');
+    } else {
+      sidebar.addClass('hidden');
+      main.addClass('full');
+    }
+  }
+
+  function showOrHideMenu() {
+    var w = $(window).width();
+    if (w >= 1280) {
+      $(window).off('mousemove', mouseMoveHandler);
+      sidebar.removeClass('hidden');
+      main.removeClass('full');
+    } else if (w < 1280 && w > 900) {
+      $(window).off('mousemove', mouseMoveHandler)
+        .on('mousemove', mouseMoveHandler);
+    } else {
+      $(window).off('mousemove', mouseMoveHandler);
+      sidebar.addClass('hidden');
+      main.addClass('full');
+    }
+  }
+  showOrHideMenu();
+
+  $(window).on('resize', showOrHideMenu);
+
+  $(".lazy").unveil(null, function () {
+    $(this).load(function () {
+      this.style.opacity = 1;
+    });
+  });
+
 })(jQuery, window, document);
